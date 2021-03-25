2b78b1926773bf1cb245ff8ff38f4a2fdd898fcd
wkuziow <wkuziow@gmail.com>
2021-03-25 21:58:33 +0100

    hello bean'

---
+++ src/main/java/pl/kuziow/restfulwebservicesmicroservices/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package pl.kuziow.restfulwebservicesmicroservices;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/pl/kuziow/restfulwebservicesmicroservices/HelloWorldController.java
+++ src/main/java/pl/kuziow/restfulwebservicesmicroservices/HelloWorldController.java
@@ -1,5 +1,20 @@
 package pl.kuziow.restfulwebservicesmicroservices;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
 
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public String helloWorld() {
+        return "Hello world";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world");
+    }
 }
